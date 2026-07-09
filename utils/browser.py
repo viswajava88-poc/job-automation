928b0a2eed5494d883d97172ffddf93011318ce5
viswajava88-poc <viswa.java88@gmail.com>
2026-07-09 16:22:17 +0530

    Refactor get_browser_options to always return headless True
    Updated get_browser_options to always return headless True with additional arguments.

--- utils/browser.py
+++ utils/browser.py
@@ -2,17 +2,13 @@ import os
 
 def is_ci() -> bool:
     return os.getenv("CI") == "true" or os.getenv("GITHUB_ACTIONS") == "true"
-
-def get_browser_options() -> dict:
-    if is_ci():
-        return {
-            "headless": True,
-            "args": [
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--disable-dev-shm-usage",
-                "--disable-gpu"
-            ]
-        }
-    else:
-        return {"headless": False}
+def get_browser_options():
+    return {
+        "headless": True, # Or False if running locally
+        "args": [
+            "--disable-blink-features=AutomationControlled", # Hides the 'navigator.webdriver' flag
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--window-size=1280,800"
+        ]
+    }
